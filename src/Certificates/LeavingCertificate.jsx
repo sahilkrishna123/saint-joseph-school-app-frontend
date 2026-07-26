4aea666be402a880bac4b917e5f0517eee1adeec
sahilkrishna123 <sahilkrishna1243@gmail.com>
2026-07-26 14:33:25 +0500

    Update styling

--- src/Certificates/LeavingCertificate.jsx
+++ src/Certificates/LeavingCertificate.jsx
@@ -116,60 +116,60 @@ export default function LeavingCertificate({ student, certType }) {
                     width: '100%',
                     height: '100%',
                     objectFit: 'fill',   // must be 'fill' so overlay aligns exactly
-                    display: 'none', // make display none 
+                    display: 'block', // make display none 
                 }}
             />
 
             {/* Left Side */}
-            <FieldLeft top={38} left={8.5} value={safe(student.grNumber)} />
-            <FieldLeft top={38} left={27.4} value={safe(student.seatNumber)} />
+            <FieldLeft top={38.5} left={8.5} value={safe(student.grNumber)} />
+            <FieldLeft top={38.5} left={27.4} value={safe(student.seatNumber)} />
 
-            <FieldLeft top={41.4} left={15} value={safe(student.fullName)} />
-            <FieldLeft top={44.5} left={15} value={safe(student.fatherName)} />
-            <FieldLeft top={44.5} left={25} value={safe(student.surname)} />
+            <FieldLeft top={41.9} left={15} value={safe(student.fullName)} />
+            <FieldLeft top={45} left={15} value={safe(student.fatherName)} />
+            <FieldLeft top={45} left={25} value={safe(student.surname)} />
 
-            <FieldLeft top={48} left={12} value={safe(student.placeOfBirth)} />
-            <FieldLeft top={51} left={25.7} value={safe(student.dateOfBirth)} />
-            <FieldLeft top={54} left={13} value={dateToWords(student.dateOfBirth)} />
+            <FieldLeft top={48.5} left={12} value={safe(student.placeOfBirth)} />
+            <FieldLeft top={52} left={25.7} value={safe(student.dateOfBirth)} />
+            <FieldLeft top={52.6} left={13.4} value={dateToWords(student.dateOfBirth)} />
 
 
-            <FieldLeft top={57.5} left={11} value={safe(student.lastSchoolAttended)} />
-            <FieldLeft top={60.7} left={13} value={safe(student.dateOfAdmission)} />
-            <FieldLeft top={63.7} left={15} value={safe(student.progessInStudies)} />
-            <FieldLeft top={63.7} left={27} value={safe(student.conduct)} />
-            <FieldLeft top={67} left={20} value={safe(student.dateOfLeaving)} />
-            <FieldLeft top={70} left={29} value="2026" />
-            <FieldLeft top={73} left={14.4} value={safe(student.seatNumber)} />
-            <FieldLeft top={87.5} left={6.5} value="26-07-2026" />
-            <FieldLeft top={73.1} left={25} value={safe(student.grade)} />
+            <FieldLeft top={58} left={11.5} value={safe(student.lastSchoolAttended)} />
+            <FieldLeft top={61.7} left={13} value={safe(student.dateOfAdmission)} />
+            <FieldLeft top={64.9} left={15} value={safe(student.progessInStudies)} />
+            <FieldLeft top={64.9} left={27} value={safe(student.conduct)} />
+            <FieldLeft top={68.5} left={20} value={safe(student.dateOfLeaving)} />
+            <FieldLeft top={72} left={29} value="2026" />
+            <FieldLeft top={74.5} left={13.7} value={safe(student.seatNumber)} />
+            <FieldLeft top={74.5} left={25} value={safe(student.grade)} />
 
+            <FieldLeft top={89.5} left={6.5} value="26-07-2026" />
 
             {/* Right Side */}
-            <Field top={36} left={45} value={safe(student.grNumber)} />
-            <Field top={36} left={85} value={safe(student.seatNumber)} />
+            <Field top={36.2} left={45} value={safe(student.grNumber)} />
+            <Field top={36.2} left={85} value={safe(student.seatNumber)} />
 
             {student.leavingCertificateIssued && (
                 <Field top={36.6} left={60} value="DUPLICATE" />
             )}
 
-            <Field top={39} left={62} value={safe(student.fullName)} />
+            <Field top={39.3} left={62} value={safe(student.fullName)} />
             <Field top={42.7} left={50} value={safe(student.fatherName)} />
-            <Field top={42.7} left={80} value={safe(student.surname)} />
-            <Field top={46} left={65} value={safe(student.placeOfBirth)} />
-            <Field top={49.3} left={75} value={safe(student.dateOfBirth)} />
-            <Field top={52.4} left={53} value={dateToWords(student.dateOfBirth)} />
+            <Field top={42.8} left={80} value={safe(student.surname)} />
+            <Field top={46.3} left={65} value={safe(student.placeOfBirth)} />
+            <Field top={49.7} left={75} value={safe(student.dateOfBirth)} />
+            <Field top={52.9} left={53} value={dateToWords(student.dateOfBirth)} />
 
-            <Field top={56} left={60} value={safe(student.lastSchoolAttended)} />
-            <Field top={59} left={60} value={safe(student.dateOfAdmission)} />
-            <Field top={62.7} left={60} value={safe(student.progessInStudies)} />
-            <Field top={62.7} left={78} value={safe(student.conduct)} />
-            <Field top={65} left={65} value={safe(student.dateOfLeaving)} />
-            <Field top={68} left={83} value="2026" />
-            <Field top={71} left={52.4} value={safe(student.seatNumber)} />
+            <Field top={56.4} left={60} value={safe(student.lastSchoolAttended)} />
+            <Field top={59.5} left={60} value={safe(student.dateOfAdmission)} />
+            <Field top={63} left={60} value={safe(student.progessInStudies)} />
+            <Field top={63} left={78} value={safe(student.conduct)} />
+            <Field top={66.5} left={65} value={safe(student.dateOfLeaving)} />
+            <Field top={69.7} left={83} value="2026" />
+            <Field top={72.5} left={52.4} value={safe(student.seatNumber)} />
 
-            <Field top={71} left={73.5} value={safe(student.grade)} />
+            <Field top={72.5} left={73} value={safe(student.grade)} />
 
-            <Field top={84} left={42.5} value="26-07-2026" />
+            <Field top={86.4} left={42.5} value="26-07-2026" />
 
         </div>
 
